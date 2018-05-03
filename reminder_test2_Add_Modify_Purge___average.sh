51b6aa847617558e4efbe356a78120833a0f1010
kirgent <kirgent@gmail.com>
2018-05-03 18:03:26 +0300

    update

--- reminder_test2_Add_Modify_Purge___average.sh
+++ reminder_test2_Add_Modify_Purge___average.sh
@@ -2,5 +2,5 @@
 java -ea -Dfile.encoding=UTF-8 -classpath \
 ./target/test-classes:\
 ./target/classes:\
-./lib/*:\
+./lib/* \
 com.intellij.rt.execution.junit.JUnitStarter -ideVersion5 -junit5 tv.zodiac.dev.testAMS_Reminder_Add_Modify_Delete_Purge__average,test2_Add_Modify_Purge___average
