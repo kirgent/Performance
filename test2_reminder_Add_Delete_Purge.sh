09f9ac1febef5d9c7fbf0fdc6cd51dffb6f81b56
kirgent <kirgent@gmail.com>
2018-05-10 21:27:48 +0300

    update

--- test2_reminder_Add_Delete_Purge.sh
+++ test2_reminder_Add_Delete_Purge.sh
@@ -6,4 +6,4 @@ java -ea -Dfile.encoding=UTF-8 -classpath \
 com.intellij.rt.execution.junit.JUnitStarter \
 -ideVersion5 \
 -junit5 \
-tv.zodiac.dev.testAMS_newAPI_Performance,test2_Add_Modify_Purge
+tv.zodiac.dev.testAMS_newAPI_Performance,test2_Add_Delete_Purge
