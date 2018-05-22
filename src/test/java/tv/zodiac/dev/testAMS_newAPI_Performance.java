e54f3945c3304bd59e645feb20fc758df5cfb3d7
kirgent <kirgent@gmail.com>
2018-05-22 19:15:28 +0300

    update

--- src/test/java/tv/zodiac/dev/testAMS_newAPI_Performance.java
+++ src/test/java/tv/zodiac/dev/testAMS_newAPI_Performance.java
@@ -20,7 +20,7 @@ class testAMS_newAPI_Performance extends API_common {
     Integer[] rack_channels_moto = { 2 };
 
     @ParameterizedTest
-    @CsvFileSource(resources = "reminders.csv", numLinesToSkip = 1)
+    @CsvFileSource(resources = "/reminders.csv", numLinesToSkip = 1)
     //@CsvSource({ "ams_ip, 172.30.81.4", "macaddress, 123456789012", "count_reminders, 3", "count_iterations" })
     void test1_Add_Purge(String ams_ip, String boxname, String macaddress, int count_reminders, int reminderChannelNumber, long reminderOffset, long reminderOffset_new, int count_iterations) throws InterruptedException, IOException {
         assertNotNull(ams_ip);
@@ -79,7 +79,7 @@ class testAMS_newAPI_Performance extends API_common {
     }
 
     @ParameterizedTest
-    @CsvFileSource(resources = "reminders.csv", numLinesToSkip = 1)
+    @CsvFileSource(resources = "/reminders.csv", numLinesToSkip = 1)
     void test2_Add_Delete_Purge(String ams_ip, String boxname, String macaddress, int count_reminders, int reminderChannelNumber, long reminderOffset, long reminderOffset_new, int count_iterations) throws IOException, InterruptedException {
             assertNotNull(ams_ip);
             assertNotNull(macaddress);
@@ -151,7 +151,7 @@ class testAMS_newAPI_Performance extends API_common {
     }
 
     @ParameterizedTest
-    @CsvFileSource(resources = "reminders.csv", numLinesToSkip = 1)
+    @CsvFileSource(resources = "/reminders.csv", numLinesToSkip = 1)
     void test3_Add_Modify_Delete_Purge(String ams_ip, String boxname, String macaddress, int count_reminders, int reminderChannelNumber, long reminderOffset, long reminderOffset_new, int count_iterations) throws IOException, InterruptedException {
             assertNotNull(ams_ip);
             assertNotNull(macaddress);
