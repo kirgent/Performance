c9dc27e061d4bd5b849e16705e8afc6b462e3d64
kirill.grushin <kirgent@gmail.com>
2018-06-01 13:41:28 +0300

    auto commit

--- test1_reminder_Add_Purge.sh
+++ test1_reminder_Add_Purge.sh
@@ -2,7 +2,7 @@
 java -ea -Dfile.encoding=UTF-8 -classpath \
 ./target/test-classes:\
 ./target/classes:\
-./lib/* \
+./libs/* \
 com.intellij.rt.execution.junit.JUnitStarter \
 -ideVersion5 \
 -junit5 \
