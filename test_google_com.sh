59ed5abfb11b776b4d0b2b07d7872c58a4ef95bb
kirill.grushin <kirgent@gmail.com>
2018-07-20 15:51:31 +0300

    auto commit

---
+++ test_google_com.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+java -ea -Dfile.encoding=UTF-8 -classpath \
+./target/test-classes:\
+./target/classes:\
+./libs/* \
+com.intellij.rt.execution.junit.JUnitStarter \
+-ideVersion5 \
+-junit5 \
+tv.zodiac.dev.testPerformance,\
+test1_google_com
